8bddba80d59b662be72c1f650023578b91d32780
l-yang-05 <40499685+l-yang-05@users.noreply.github.com>
2019-10-03 17:13:44 -0400

    Added more comments to server.js

--- server.js
+++ server.js
@@ -13,18 +13,19 @@ const bodyParser = require('body-parser')
 const app = express()
 const PORT = process.env.port || 4000
 
-// Using app.use to use the bodyParser module I'm importing
+/* Using app.use to use the bodyParser module I'm importing. Helps with parsing the res.body to json object so it can be rendered and manipulated
+through client */
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-// Using helmet
+// Helmet helps apply sercurity to application by adding http headers on the responses.
 app.use(helmet())
 
 // Create variable with file, then use that variable with morgan to write stream log in file.
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
 app.use(morgan('dev', { stream: accessLogStream }))
 
-// Import route files and assigning them to a variable
+// Import route files and assigning them to a variable so I can use them in app.use()
 const rootAPI = require('./routes/rootAPI')
 const productsAPI = require('./routes/apiproducts')
 const productFilter = require('./routes/productfilter')
@@ -45,4 +46,5 @@ app.listen(PORT, () => {
     console.log(`Magic happens on port ${PORT}!`)
 })
 
+// Exporting the app to be used for testing the API endpoints
 module.exports = app;
